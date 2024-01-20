43c2dbe9c5c678deb226f4771467968445bfe0af
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-01-20 23:02:41 +0600

    Added widgets
    Scaffold and appbar .

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,22 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    //material widget
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Homepage"),
+      ),
+      body: Center(
+          //center widget
+
+          child: Text(
+        'Flutter Is awesome',
+        style: TextStyle(
+          fontSize: 30.0
+        ),
+        textDirection: TextDirection.ltr,
+      )),
+    ),
+  ));
+}
