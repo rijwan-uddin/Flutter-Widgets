d7a9b461c9ba266a41d3c38a8340c73566254043
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-01-20 23:25:40 +0600

    Added image network widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-
+const url='https://carsguide-res.cloudinary.com/image/upload/f_auto%2Cfl_lossy%2Cq_auto%2Ct_default/v1/editorial/listicle/hero_image/2020-Bugatti%20Centodieci-1001x565-%282%29.jpg';
 void main() {
   runApp(MaterialApp(
     //material widget
     home: Scaffold(
       appBar: AppBar(
-        title: Text("Homepage"),
+        title: Text("Cars"),
       ),
-      body: Center(
+      body:  Center(
           //center widget
 
-          child: Text(
-        'Flutter Is awesome',
-        style: TextStyle(
-          fontSize: 30.0
-        ),
-        textDirection: TextDirection.ltr,
-      )),
+          child:Image.network(url,height: 1000,width: 2000,),
+      ),
     ),
   ));
 }
