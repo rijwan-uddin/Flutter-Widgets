67c233bc27f995d7e36ebfa7c0b0669ecc9816a6
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-01-27 22:56:31 +0600

    Image from network

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 const url='https://carsguide-res.cloudinary.com/image/upload/f_auto%2Cfl_lossy%2Cq_auto%2Ct_default/v1/editorial/listicle/hero_image/2020-Bugatti%20Centodieci-1001x565-%282%29.jpg';
+const url2='https://www.motortrend.com/uploads/2022/08/2022-Bugatti-Chiron-Super-Sport-2-1.jpg?fit=around%7C875:492';
 void main() {
   runApp(MaterialApp(
     //material widget
@@ -10,7 +11,7 @@ void main() {
       body:  Center(
           //center widget
 
-          child:Image.network(url,height: 1000,width: 2000,),
+          child:Image.network(url, fit: BoxFit.cover,),//show image from network
       ),
     ),
   ));
